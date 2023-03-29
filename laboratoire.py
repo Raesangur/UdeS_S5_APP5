7762e269be7bde1771d0db3d7ece324c028c1b1a
Pascal-Emmanuel Lachance <17782420+Raesangur@users.noreply.github.com>
2023-03-29 16:22:12 -0400

    Update laboratoire.py

--- laboratoire.py
+++ laboratoire.py
@@ -1,12 +1,43 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def exercice3(mu, ):
+
+def Fx(x):
+    if x >= 0:
+        return 0.5 * (1 + np.sqrt(1 - np.exp(-1 * x**2 * np.sqrt(np.pi / 8))))
+    else:
+        return 1 - Fx(-x)
+
+def x(Fx):
+    if Fx >= 0.5:
+        return np.sqrt(-np.log(-1 * (2 * Fx - 1)**2 + 1)/np.sqrt(np.pi / 8))
+    else:
+        return -np.sqrt(-np.log(-1 * (2 * Fx - 1)**2 + 1)/np.sqrt(np.pi / 8))
+
+def exercice4(mu = 10, std = 2):
+    # Generating random numbers
+    N = 10000
+    _fx = [Fx(_x) for _x in np.linspace(-5, 5, N)]
+
+    plt.plot(_fx)
+    plt.show()
+
+    N = 10000
+    vals = np.random.rand(N)
+
+    y = [x(_Fx) for _Fx in vals]
+    plt.hist(y, 100)
+    plt.show()
+
+
+
+
+def exercice3(mu = 10, std = 2):
     # Generating random numbers
     N  = 10000
     U1 = np.random.rand(N)
     U2 = np.random.rand(N)
-    X  = 1- + 2 * np.cos(2 * np.pi * U1) * np.sqrt(-2 * np.log(U2))
+    X  = mu + std * np.cos(2 * np.pi * U1) * np.sqrt(-2 * np.log(U2))
 
     # Displaying histogram and relative frequency plot
     fig, ax1 = plt.subplots()
@@ -75,4 +106,4 @@ def exercice1():
     print(mse)
 
 if __name__ == '__main__':
-    exercice3()
+    exercice4()
