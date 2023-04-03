e8cff361092b99ffa3ba6e6a2dfcdbf8baf31b4e
raesangur <pascalemmanuel.lachance@gmail.com>
2023-04-03 10:49:06 -0400

    #4.1

--- p4.py
+++ p4.py
@@ -29,38 +29,43 @@ def _r(Fr, sigma):
     if (Fr <= 0):
         return 0
     else:
-        return 2 * sigma * np.log(Fr)
+        return np.sqrt(-2 * sigma * np.log(-Fr + 1))
 
 def main():
     N     = 10000
     U     = np.random.rand(N)
+    U1    = np.random.rand(N)
     theta = U * np.pi * 2
 
     # Displaying histogram
-    plt.hist(U, align="left")[0]
+    fig, axs = plt.subplots(2)
+    axs[0].hist(theta, align="left")[0]
+    axs[1].hist(U1, align="left")[0]
     plt.show()
 
     # Calculating r
     sigmar = [0.25, 1, 4, 9, 16]
 
+    fig, axs = plt.subplots(3)
     fr = [[_fr(r, sigma) for r in np.linspace(0, 10, 1000)] for sigma in sigmar]
-    [plt.plot(r) for r in fr]
-    plt.show()
+    [axs[0].plot(r) for r in fr]
+    axs[0].set_title("f(r)")
 
     Fr = [[_Fr(r, sigma) for r in np.linspace(0, 10, 1000)] for sigma in sigmar]
-    [plt.plot(r) for r in Fr]
-    plt.show()
+    [axs[1].plot(r) for r in Fr]
+    axs[1].set_title("F(r)")
 
     r = [[_r(Fr, sigma) for Fr in np.linspace(0, 1, 100)] for sigma in sigmar]
-    [plt.plot(R) for R in r]
+    [axs[2].plot(R) for R in r]
+    axs[2].set_title("r(Fr)")
     plt.show()
-
+    
     Ur = np.random.rayleigh(4, N)
     D  = []
     Dr = []
     phi = 15 * np.pi / 180
     for i in range(N):
-        D. append(monte_carlo(50, phi, theta[i], _r(U[i], 4)))
+        D. append(monte_carlo(50, phi, theta[i], _r(U1[i], 4)))
         Dr.append(monte_carlo(50, phi, theta[i], Ur[i]))
 
     fig, axs = plt.subplots(2)
@@ -68,6 +73,10 @@ def main():
     axs[1].hist(Dr)
     axs[0].set_title("Distribution avec fonction r inverse")
     axs[1].set_title("Distribution avec fonction rayleigh")
+    plt.show()
+
+    plt.scatter(theta)
+    plt.scatter(U1)
     plt.show()
 
 if __name__ == '__main__':
